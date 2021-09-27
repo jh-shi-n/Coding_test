444524dbc278ba5cd64aa37a10403c700547e21d
Shin_JH <61272439+shinjh-dl@users.noreply.github.com>
2021-09-27 15:04:52 +0900

    Update Code_수포자.py

--- Code_수포자.py
+++ Code_수포자.py
@@ -1,5 +1,5 @@
 #1차 시도 _ 정확도 20%
-"""
+''' Python
 def solution(answers):
     scores = [0,0,0]
     answer = []
@@ -22,11 +22,11 @@ def solution(answers):
             answer.append(i+1)
     
     return answer
-"""
+'''
 
 
 #2차 시도 _ 정확도 100%
-"""
+''' Python
 def solution(answers):
     scores = [0,0,0]
     pt1 = [1,2,3,4,5]
@@ -47,4 +47,4 @@ def solution(answers):
             result.append(j+1)
     
     return result
-"""
+'''
