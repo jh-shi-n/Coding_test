04387ebb49a3734da7893041d412fc940583b211
Shin_JH <61272439+shinjh-dl@users.noreply.github.com>
2021-10-14 13:38:38 +0900

    Create Code_K번째수.py

---
+++ Programmers/Code_K번째수.py
@@ -0,0 +1,10 @@
+def solution(array, commands):
+    answer = []
+
+    for i in range(len(commands)):
+        arr_list = array[commands[i][0]-1:commands[i][1]]
+        arr_list.sort()
+
+        answer.append(arr_list[commands[i][2]-1])
+    
+    return answer
