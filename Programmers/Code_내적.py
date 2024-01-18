b0581ace339c90e195dbe4be6a91c2f53a7f7343
jh-shi-n <sj124h@naver.com>
2024-01-18 23:55:12 +0900

    file move

--- Programmers/Code_내적.py
+++
@@ -1,9 +0,0 @@
-def solution(a, b):
-    sum_list =[]
-    
-    for i in range(len(a)):
-        temp = (a[i]*b[i])
-        sum_list.append(temp)
-    
-    answer = sum(sum_list)
-    return answer
