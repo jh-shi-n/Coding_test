958d8846183068e7034d60bf458961343e859aed
JoonHyeok Shin <61272439+jh-shi-n@users.noreply.github.com>
2024-04-21 17:35:40 +0900

    upload answer_9095

---
+++ coding_test/Baekjoon/Code_123더하기.py
@@ -0,0 +1,16 @@
+# 재귀함수 작성을 통해 계속적으로 값에 대한 비교 진행
+def sum_number(x):
+    if x==1:
+      return 1
+    elif x==2:
+      return 2
+    elif x==3:
+      return 4
+    else:
+      return sum_number(x-1) + sum_number(x-2) + sum_number(x-3)
+
+# 몇개의 값을 입력받을 것인지, 그리고 입력된 값마다 재귀함수에 입력하여 결과값 도출
+N = int(input())
+for _ in range(N):
+    target = int(input())
+    print(sum_number(target))
